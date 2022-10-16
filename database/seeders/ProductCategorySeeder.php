4c532f381196da7cf1d85ac3fcdef435bdb62a4e
Danny Wahyudi <wahyudidanny23@gmail.com>
2022-10-16 14:58:00 +0700

    Create ProductCategorySeeder.php

---
+++ database/seeders/ProductCategorySeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use Faker\Factory as Faker;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class ProductCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
