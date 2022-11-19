d7ee293b7a46cd032f81b474b6b85182d05cc06f
Project-web-code-program <scooptechcorp@gmail.com>
2022-11-19 16:59:06 +0700

    Update ProductSell.php

--- app/Models/ProductSell.php
+++ app/Models/ProductSell.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductSell extends Model
 {
-    protected $table = "ProductSells";
+    protected $table = "productSells";
 
     protected $dates = ['created_at', 'DeletedAt'];
 
