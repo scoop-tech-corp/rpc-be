9906548a1a44d7faac507fc51bb5d96c3db5a13f
Danny Wahyudi <wahyudidanny23@gmail.com>
2022-12-30 00:02:31 +0700

    update Case Sensitive Table
    update Case Sensitive Table

--- database/migrations/2022_11_27_152621_create_table_role_access.php
+++ database/migrations/2022_11_27_152621_create_table_role_access.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('tableroleaccess', function (Blueprint $table) {
+        Schema::create('tableRoleAccess', function (Blueprint $table) {
             $table->id();
             $table->string('accessType');
             $table->timestamps();
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tableroleaccess');
+        Schema::dropIfExists('tableRoleAccess');
     }
 };
