46ded0a4ce2a480b97a2908ea884f45d6357a074
Danny Wahyudi <wahyudidanny23@gmail.com>
2023-01-31 20:21:19 +0700

    Update DataFacilityAll.php

--- app/Exports/Facility/DataFacilityAll.php
+++ app/Exports/Facility/DataFacilityAll.php
@@ -62,13 +62,13 @@ class DataFacilityAll implements FromCollection, ShouldAutoSize, WithHeadings, W
             ->where([['a.isDeleted', '=', '0']]);
 
 
-        if ($this->locationId) {
+        // if ($this->locationId) {
 
-            if (sizeof($this->locationId)) {
-                $data = $data->whereIn('a.id', $this->locationId);
-            }
+        //     if (sizeof($this->locationId)) {
+        //         $data = $data->whereIn('a.id', $this->locationId);
+        //     }
             
-        }
+        // }
 
         if ($this->search) {
             $res = $this->Search($this->search);
