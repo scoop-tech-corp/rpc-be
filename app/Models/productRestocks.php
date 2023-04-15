7f778f7bb75f862d1df66c28cd32f54d8851bde4
Project-web-code-program <amx23baradja@gmail.com>
2023-04-16 01:25:30 +0700

    Add Create Restock Product

---
+++ app/Models/productRestocks.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class productRestocks extends Model
+{
+    protected $table = "productRestocks";
+
+    protected $dates = ['created_at', 'deletedAt'];
+
+    protected $guarded = ['id'];
+
+    protected $fillable = ['userId', 'userUpdateId'];
+}
---
+++ database/migrations/2023_04_15_150217_create_product_restock_details_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productRestockDetails', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('productRestockId');
+            $table->integer('productId');
+            $table->string('productType');
+            $table->integer('supplierId');
+            $table->date('requireDate');
+            $table->integer('reStockQuantity');
+            $table->decimal('costPerItem',$precision =18, $scale=2);
+            $table->string('remark');
+
+            $table->boolean('isDeleted')->nullable()->default(false);
+            $table->integer('userId');
+            $table->integer('userUpdateId')->nullable();
+            $table->string('deletedBy')->nullable();
+            $table->timestamp('deletedAt', 0)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productRestockDetails');
+    }
+};
