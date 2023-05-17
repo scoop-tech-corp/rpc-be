be86b2780e65e5b332ee994ce47764ebc8aed2a3
Danny Wahyudi <wahyudidanny23@gmail.com>
2023-05-17 21:39:44 +0700

    push notification
    push notification

---
+++ database/migrations/2023_03_28_113735_create_table_push_notifications.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pushNotifications', function (Blueprint $table) {
+            $table->id();
+            $table->integer('usersId');
+            $table->string('menuName');
+            $table->string('message');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pushNotifications');
+    }
+};
