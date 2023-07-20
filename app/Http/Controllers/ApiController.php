e1ea59a56cd84b700d145e3bfe591ca2bcb6d8db
Danny Wahyudi <wahyudidanny23@gmail.com>
2023-07-20 20:15:33 +0700

    delete all info set api controller
    delete all info set api controller

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -113,7 +113,7 @@ class ApiController extends Controller
 
                 $userId = $checkIfValueExits->usersId;
                 $emailaddress = $checkIfValueExits->email;
-                info("masuk kesini 4");
+
                 $users = DB::table('users')
                     ->leftjoin('jobTitle', 'jobTitle.id', '=', 'users.jobTitleId')
                     ->leftjoin('usersRoles', 'usersRoles.id', '=', 'users.roleId')
@@ -130,9 +130,7 @@ class ApiController extends Controller
                         ['jobTitle.isActive', '=', '1']
                     ])
                     ->first();
-                info("masuk kesini 3");
-                info($users->roleName);
-                info($userId);
+
                 $data = DB::table('accessControl as a')
                     ->join('menuList as b', 'b.id', '=', 'a.menuListId')
                     ->join('accessType as c', 'c.id', '=', 'a.accessTypeId')
@@ -143,7 +141,6 @@ class ApiController extends Controller
                     ->where([['a.roleId', '=', $users->roleId],])
                     ->get();
 
-                info("masuk kesini");
                 $accessLimit = DB::table('accessControl as a')
                     ->join('menuList as b', 'b.id', '=', 'a.menulistId')
                     ->join('accessLimit as c', 'c.id', '=', 'a.accessLimitId')
@@ -156,9 +153,6 @@ class ApiController extends Controller
                     ->where([['a.roleId', '=', $users->roleId],])
                     ->get();
 
-                info("masuk kesini 2");
-                info($accessLimit);
-                info($data);
                 return response()->json([
                     'success' => true,
                     'token' => $token,
