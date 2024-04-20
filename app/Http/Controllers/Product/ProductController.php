ae64c58245b4e2e69808bd006586c989bdd8079d
Project-web-code-program <amx23baradja@gmail.com>
2024-04-20 16:21:00 +0700

    Update ProductController.php

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -688,8 +688,7 @@ class ProductController
 
     public function ListProductClinicWithLocation(Request $request)
     {
-        $request->locationId = json_decode($request->locationId);
-        if ($request->locationId == 'all') {
+        if (count($request->locationId) == 0) {
 
             $data = DB::table('productClinics as pc')
                 ->join('productClinicLocations as pcl', 'pc.id', 'pcl.productClinicId')
