45945c81874b618cd69072ce2b36fdddf881053b
Project-web-code-program <amx23baradja@gmail.com>
2025-05-25 15:09:23 +0700

    Fixing Pet Check Condition

--- app/Http/Controllers/Transaction/TransPetClinicController.php
+++ app/Http/Controllers/Transaction/TransPetClinicController.php
@@ -885,18 +885,18 @@ class TransPetClinicController extends Controller
             'petCheckRegistrationNo' => 'required|string',
 
             'isAnthelmintic' => 'required|boolean',
-            'anthelminticDate' => 'required|date',
-            'anthelminticBrand' => 'required|string',
+            'anthelminticDate' => 'nullable|date',
+            'anthelminticBrand' => 'nullable|string',
 
             'isVaccination' => 'required|boolean',
-            'vaccinationDate' => 'required|date',
-            'vaccinationBrand' => 'required|string',
+            'vaccinationDate' => 'nullable|date',
+            'vaccinationBrand' => 'nullable|string',
 
             'isFleaMedicine' => 'required|boolean',
-            'fleaMedicineDate' => 'required|date',
-            'fleaMedicineBrand' => 'required|string',
+            'fleaMedicineDate' => 'nullable|date',
+            'fleaMedicineBrand' => 'nullable|string',
 
-            'previousAction' => 'required|string',
+            'previousAction' => 'nullable|string',
             'othersCompalints' => 'nullable|string',
 
             'weight' => 'required|numeric',
@@ -907,30 +907,30 @@ class TransPetClinicController extends Controller
             'temperatureTop' => 'required|numeric',
             'temperatureCategory' => 'required|integer',
 
-            'isLice' => 'required|boolean',
+            'isLice' => 'nullable|boolean',
             'noteLice' => 'nullable|string',
 
-            'isFlea' => 'required|boolean',
+            'isFlea' => 'nullable|boolean',
             'noteFlea' => 'nullable|string',
 
-            'isCaplak' => 'required|boolean',
+            'isCaplak' => 'nullable|boolean',
             'noteCaplak' => 'nullable|string',
 
-            'isTungau' => 'required|boolean',
+            'isTungau' => 'nullable|boolean',
             'noteTungau' => 'nullable|string',
 
             'ectoParasitCategory' => 'nullable|integer',
 
-            'isNematoda' => 'required|boolean',
+            'isNematoda' => 'nullable|boolean',
             'noteNematoda' => 'nullable|string',
             //endoparasit
-            'isTermatoda' => 'required|boolean',
+            'isTermatoda' => 'nullable|boolean',
             'noteTermatoda' => 'nullable|string',
 
-            'isCestode' => 'required|boolean',
+            'isCestode' => 'nullable|boolean',
             'noteCestode' => 'nullable|string',
 
-            'isFungiFound' => 'required|boolean',
+            'isFungiFound' => 'nullable|boolean',
 
             'konjung' => 'nullable|string',
             'ginggiva' => 'nullable|string',
@@ -946,7 +946,7 @@ class TransPetClinicController extends Controller
             'lokomosiFindings' => 'nullable|string',
 
             //ingus
-            'isSnot' => 'required|boolean',
+            'isSnot' => 'nullable|boolean',
             'noteSnot' => 'nullable|string',
 
             'breathType' => 'nullable|integer',
@@ -986,7 +986,7 @@ class TransPetClinicController extends Controller
             'othersFoundVision' => 'nullable|string',
 
             'earlobe' => 'nullable|string',
-            'earwax' => 'required|integer',
+            'earwax' => 'nullable|integer',
             'earwaxCharacter' => 'nullable|string',
             'othersFoundEar' => 'nullable|string',
 
