67dc98685b4790cb08b6b35c07353086c8b2a45e
Project-web-code-program <amx23baradja@gmail.com>
2025-06-10 00:30:04 +0700

    Add Transaction Pet Salon

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -20,6 +20,8 @@ use App\Models\TransactionPetClinic;
 use App\Models\TransactionPetClinicLog;
 use App\Models\TransactionPetHotel;
 use App\Models\TransactionPetHotelLog;
+use App\Models\transactionpetsalon;
+use App\Models\transactionPetSalonLog;
 
 if (!function_exists('adminAccess')) {
     function adminAccess($id)
@@ -273,6 +275,18 @@ if (!function_exists('transactionBreedingLog')) {
     }
 }
 
+if (!function_exists('transactionPetSalonLog')) {
+    function transactionPetSalonLog($transactionId, $activity, $remark, $userId)
+    {
+        transactionPetSalonLog::create([
+            'transactionId' => $transactionId,
+            'activity' => $activity,
+            'remark' => $remark,
+            'userId' => $userId,
+        ]);
+    }
+}
+
 if (!function_exists('statusTransactionPetClinic')) {
     function statusTransactionPetClinic($transactionId, $status)
     {
@@ -307,6 +321,16 @@ if (!function_exists('statusTransactionBreeding')) {
     function statusTransactionBreeding($transactionId, $status)
     {
         TransactionBreeding::where('id', '=', $transactionId)
+            ->update([
+                'status' => $status,
+            ]);
+    }
+}
+
+if (!function_exists('statusTransactionPetSalon')) {
+    function statusTransactionPetSalon($transactionId, $status)
+    {
+        transactionpetsalon::where('id', '=', $transactionId)
             ->update([
                 'status' => $status,
             ]);
