6bd1744d1c58e0f463d574892bcffae3c93083b1
Project-web-code-program <amx23baradja@gmail.com>
2025-07-12 22:36:46 +0700

    Change Image Source

--- resources/views/payroll/salary-slip.blade.php
+++ resources/views/payroll/salary-slip.blade.php
@@ -167,7 +167,7 @@
         <table class="header-table">
             <tr>
                 <td style="width: 25%; vertical-align: top; padding: 0 10px 0 0;">
-                    <img src="{{ public_path() . '/asset/logo-rpc-full-min.png' }}" style="width: 100px;" alt="Logo">
+                    <img src="{{ public_path() . '/asset/logo-rpc-full-min.webp' }}" style="width: 100px;" alt="Logo">
                 </td>
                 <td style="text-align: left; padding: 0; line-height: 1.3;">
                     <div style="font-weight: bold; font-size: 14px;">RADHIYAN PET & CARE GRUP</div>
