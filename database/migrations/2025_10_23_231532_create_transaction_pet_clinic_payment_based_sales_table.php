7c35034e3d55961194ef0cfd625e7fcfd115d7f1
Project-web-code-program <amx23baradja@gmail.com>
2025-11-18 12:06:09 +0700

    Add Payment Outpatient

---
+++ database/migrations/2025_10_23_231532_create_transaction_pet_clinic_payment_based_sales_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_pet_clinic_payment_based_sales', function (Blueprint $table) {
+            $table->id();
+            $table->integer('transactionId');
+            $table->integer('promoId');
+            $table->decimal('amountDiscount', $precision = 18, $scale = 2);
+
+            $table->boolean('isDeleted')->nullable()->default(false);
+            $table->integer('userId');
+            $table->integer('userUpdateId')->nullable();
+            $table->string('deletedBy')->nullable();
+            $table->timestamp('deletedAt', 0)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_pet_clinic_payment_based_sales');
+    }
+};
